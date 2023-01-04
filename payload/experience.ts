a5886e2bb3915a14100199df0df93397a5cdb906
jun <59019322+mrgentle1@users.noreply.github.com>
2023-01-05 00:29:02 +0900

    2023-01-05 update

--- payload/experience.ts
+++ payload/experience.ts
@@ -5,27 +5,15 @@ const experience: IExperience.Payload = {
   disableTotalPeriod: false,
   list: [
     {
-      title: 'Foo Company',
-      position: 'Node.js Developer',
-      startedAt: '2018-02',
-      descriptions: [
-        'OTA Service Provider',
-        'Data modeling and Mashup API development',
-        'Systems Engineering and DevOps',
-      ],
-      skillKeywords: ['Node.js', 'TypeScript', 'Express.js', 'Mashup API', 'AWS'],
-    },
-    {
       title: '코그콤',
-      position: '데이터 엔지니어',
+      position: '데이터 엔지니어(학부 인턴)',
       startedAt: '2021-07',
       endedAt: '2021-09',
       descriptions: [
-        'Online Commerce IT Startup',
-        'Development of reservation system and service worker',
-        'AWS infrastructure and RDBMS maintenance and development',
+        '논문 유사도 검사 서비스 알고리즘 연구',
+        '이커머스 데이터 수집 및 분석',
       ],
-      skillKeywords: ['Node.js', 'MySQL', 'AWS'],
+      skillKeywords: ['Python', 'PostgreSQL', 'Django', 'mongoDB'],
     },
   ],
 };
--- payload/project.ts
+++ payload/project.ts
@@ -4,35 +4,46 @@ const project: IProject.Payload = {
   disable: false,
   list: [
     {
-      title: 'Mashup API Development',
-      startedAt: '2018-02',
-      where: 'Foo Company',
+      title: 'ZIKIME',
+      startedAt: '2021-06',
+      endedAt: '2021-12',
+      where: '동아리 프로젝트 및 공모전',
       descriptions: [
-        { content: 'Development and operation of Mashup API of Aggregation Layer' },
-        {
-          content: 'Add Foo Feature in 2019',
-          weight: 'MEDIUM',
-          descriptions: [
-            { content: 'Integer ut libero eu libero mattis rutrum at at urna' },
-            { content: 'Integer non turpis ante' },
-          ],
-        },
-        {
-          content: 'Launched Bar Service in 2018',
-          weight: 'MEDIUM',
-          descriptions: [
-            { content: 'Suspendisse vestibulum odio id libero facilisis gravida' },
-            {
-              content: 'In ipsum est, pellentesque vestibulum tortor eu, bibendum imperdiet metus',
-            },
-          ],
-        },
+        { content: '아동과 치매노인을 위한 이동식 카메라 및 실시간 영상 스트리밍 서비스' },
+        { content: '실시간으로 사용자가 웹을 통해 카메라로 녹화된 영상을 볼 수 있도록 구현' },
+        // { content: 'https://github.com/ZIKIME2021/zikime-server',
+        // href: 'https://github.com/ZIKIME2021/zikime-server', },
       ],
     },
     {
-      title: 'Lorem ipsum Project',
-      startedAt: '2016-10',
-      endedAt: '2017-11',
+      title: 'LookOut!',
+      startedAt: '2022-07',
+      endedAt: '2022-12',
+      where: '동아리 프로젝트 및 공모전',
+      descriptions: [
+        { content: '청각장애인들에게 화재나 교통사고 발생시 위험한 소리를 인식하여 앱을 통해 진동 알림을 전달하는 서비스' },
+        { content: '위험 말소리를 학습시켜서 모델 생성, 텐서플로우 라이트로 변환하여 기기에서 실시간으로 추론이 가능하도록 구현' },
+        { content: 'https://github.com/KOSS-LOOK-OUT/LookOut_Device',
+          href: 'https://github.com/KOSS-LOOK-OUT/LookOut_Device', },
+      ],
+    },
+    {
+      title: 'Bzero',
+      startedAt: '2022-07',
+      endedAt: '2022-09',
+      where: '동아리 프로젝트 및 공모전',
+      descriptions: [
+        { content: '제로웨이스터들을 위한 웹 플랫폼 서비스로, 로그인과 회원정보 관리 API 개발을 담당함' },
+        { content: '' },
+        { content: '회원정보 페이지의 한줄 소개, 프로필 이미지, 출석 정보 등을 고려하여 모델을 설계함' },
+        { content: 'https://bzero.cf/',
+          href: 'https://bzero.cf/', },
+      ],
+    },
+    {
+      title: 'Bzero',
+      startedAt: '2022-07',
+      endedAt: '2022-09',
       where: 'Bar Co., LTD.',
       descriptions: [
         { content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' },
